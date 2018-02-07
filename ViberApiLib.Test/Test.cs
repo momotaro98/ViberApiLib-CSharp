819cc193fd1b9b2215c1a79ab67c3816f641b27d
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-02-07 23:55:00 +0900

    test: Add test project

---
+++ ViberApiLib.Test/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace ViberApiLib.Test
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
